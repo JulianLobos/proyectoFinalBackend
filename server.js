6ea0b75bc912dbd06027dbe1ef710e83c3c116f0
julianLobos <julianign@gmail.com>
2022-09-05 22:10:04 -0300

    update

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const router = express.Router();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.metal.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = 8080;
 const app = express();
 const server = app.listen(PORT, () => {
@@ -12,3 +17,5 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use(require('./routes/main'));
+
+export default app;
